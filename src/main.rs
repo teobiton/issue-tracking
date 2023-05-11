b81acf143761c336fc882584366bbf6f60babe43
teobiton <teo.biton@gmail.com>
2023-05-11 19:40:48 +0200

    Add static json parsing functions
    A json file can be recognized and serialized in Rust structures.
    Only the fields that are needed for the output are implemented
    in the structures. Some may be added in the future if necessary.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::path::Path;
 use std::process;
 use structopt::StructOpt;
 
+mod structs;
+
 /*
     Main thread of the application.
     Arguments are processed here and external functions are called to build the output.
@@ -28,16 +30,37 @@ struct Args {
     json: String,
 }
 
-fn is_json_file_ok(json: &Path) -> i8 {
-    if !json.exists() {
+fn is_json_file_ok(filepath: &Path) -> i8 {
+    if !filepath.exists() {
         return 1;
     }
 
-    if json.extension().and_then(|ext| ext.to_str()) != Some("json") {
+    if filepath.extension().and_then(|ext| ext.to_str()) != Some("json") {
         return 2;
     }
 
     0
+}
+
+fn has_github_issues(text: &str) -> bool {
+    let fields: [&str; 7] = [
+        "\"issues\"",
+        "\"title\"",
+        "\"number\"",
+        "\"labels\"",
+        "\"state\"",
+        "\"created_at\"",
+        "\"closed_at\"",
+    ];
+
+    for field in fields {
+        if !text.contains(field) {
+            println!("{}", field);
+            return false;
+        }
+    }
+
+    true
 }
 
 fn main() -> Result<(), ExitFailure> {
@@ -59,9 +82,25 @@ fn main() -> Result<(), ExitFailure> {
         _ => {}
     };
 
+    let repository_issues = {
+        // Load the first file into a string
+        let text = std::fs::read_to_string(&json_file).unwrap();
+
+        if !has_github_issues(&text) {
+            eprintln!("'{}' does not seem to contain GitHub issues.", &args.json);
+            process::exit(1);
+        }
+        // Parse the string into a static JSON structure
+        serde_json::from_str::<structs::Repository>(&text).unwrap()
+    };
+
     // Debug
     println!("JSON input: {}", json_file.display());
     println!("Output file: {}", &args.output);
-
+    println!("Test issue name: {}", &repository_issues.issues[0].title);
+    println!(
+        "Test issue date: {}",
+        &repository_issues.issues[0].created_at
+    );
     Ok(())
 }
