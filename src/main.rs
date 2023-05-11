b106994053bc2196648e3a0c1608ec7cb77a3897
teobiton <teo.biton@gmail.com>
2023-05-11 20:19:44 +0200

    Regroup functions and structures in parser
    All structures and functions related to processing the json
    input file are declared in parser.rs.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::path::Path;
 use std::process;
 use structopt::StructOpt;
 
-mod structs;
+mod parser;
+use parser::parse_json_input;
+use parser::Repository;
 
 /*
     Main thread of the application.
@@ -42,27 +44,6 @@ fn is_json_file_ok(filepath: &Path) -> i8 {
     0
 }
 
-fn has_github_issues(text: &str) -> bool {
-    let fields: [&str; 7] = [
-        "\"issues\"",
-        "\"title\"",
-        "\"number\"",
-        "\"labels\"",
-        "\"state\"",
-        "\"created_at\"",
-        "\"closed_at\"",
-    ];
-
-    for field in fields {
-        if !text.contains(field) {
-            println!("{}", field);
-            return false;
-        }
-    }
-
-    true
-}
-
 fn main() -> Result<(), ExitFailure> {
     println!("Retrieving arguments ... ");
 
@@ -82,17 +63,7 @@ fn main() -> Result<(), ExitFailure> {
         _ => {}
     };
 
-    let repository_issues = {
-        // Load the first file into a string
-        let text = std::fs::read_to_string(&json_file).unwrap();
-
-        if !has_github_issues(&text) {
-            eprintln!("'{}' does not seem to contain GitHub issues.", &args.json);
-            process::exit(1);
-        }
-        // Parse the string into a static JSON structure
-        serde_json::from_str::<structs::Repository>(&text).unwrap()
-    };
+    let repository_issues: Repository = parse_json_input(&json_file);
 
     // Debug
     println!("JSON input: {}", json_file.display());
@@ -102,5 +73,6 @@ fn main() -> Result<(), ExitFailure> {
         "Test issue date: {}",
         &repository_issues.issues[0].created_at
     );
+    
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,63 @@
+use std::path::Path;
+use std::process;
+use serde_derive::{Deserialize, Serialize};
+
 /*
-    Several functions to process the json input.
-    Its data is analyzed and packed in structures.
+    Functions to process the json input.
+    Structs defined to store data from the json.
+    json data is analyzed and packed in structures.
 */
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Repository {
+    pub issues: Vec<Issue>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Issue {
+    pub title: String,
+    pub number: i32,
+    pub labels: Vec<Label>,
+    pub state: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub closed_at: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Label {
+    name: String,
+}
+
+fn has_github_issues(text: &str) -> bool {
+    let fields: [&str; 7] = [
+        "\"issues\"",
+        "\"title\"",
+        "\"number\"",
+        "\"labels\"",
+        "\"state\"",
+        "\"created_at\"",
+        "\"closed_at\"",
+    ];
+
+    for field in fields {
+        if !text.contains(field) {
+            println!("{}", field);
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn parse_json_input(json_file: &Path) -> Repository {
+    // Load the first file into a string
+    let text = std::fs::read_to_string(&json_file).unwrap();
+
+    if !has_github_issues(&text) {
+        eprintln!("'{}' does not seem to contain GitHub issues.", &json_file.display());
+        process::exit(1);
+    }
+    // Parse the string into a static JSON structure
+    serde_json::from_str::<Repository>(&text).unwrap()
+}
