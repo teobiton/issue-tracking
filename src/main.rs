dee4bad5a49634c783b2890581b696b231771b7e
teobiton <teo.biton@gmail.com>
2023-05-17 00:15:31 +0200

    Add filters argument

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,46 @@ struct Args {
         long = "--output",
         short = "-o",
         default_value = "",
-        help = "Specify a file to store the csv"
+        help = "Specify a file to store the csv."
     )]
     output: String,
 
+    /// --label=label
+    #[structopt(
+        long = "--label",
+        short = "-l",
+        default_value = "",
+        help = "Filter the issues based on a label."
+    )]
+    label: String,
+
+    /// --from-date=date
+    #[structopt(
+        long = "--from-date",
+        default_value = "",
+        help = "Only consider issues updated after this date. Format: YYYY-MM-DD"
+    )]
+    from_date: String,
+
+    /// --until-date=date
+    #[structopt(
+        long = "--until-date",
+        default_value = "",
+        help = "Only consider issues updated before this date. Format: YYYY-MM-DD"
+    )]
+    until_date: String,
+
+    /// --status=status
+    #[structopt(
+        long = "--status",
+        short = "-s",
+        default_value = "",
+        help = "Only consider issues that have a particular status."
+    )]
+    status: String,
+
     /// Positional argument
-    #[structopt(help = "Required JSON file")]
+    #[structopt(help = "Required JSON file.")]
     json: String,
 }
 
