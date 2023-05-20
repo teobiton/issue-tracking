d27f75493dc49ac7f7a07faa462c12b24a0b197d
teobiton <teo.biton@gmail.com>
2023-05-21 01:16:37 +0200

    Add filters arguments

--- src/input.rs
+++ src/input.rs
@@ -32,6 +32,31 @@ pub struct Args {
     /// --print-labels
     #[structopt(long, help = "Print all available labels in the repository.")]
     pub print_labels: bool,
+    
+    /// --from-date=date
+    #[structopt(
+        long = "--from-date",
+        default_value = "",
+        help = "Only consider issues updated after this date. Format: YYYY-MM-DD"
+    )]
+    pub from_date: String,
+
+    /// --until-date=date
+    #[structopt(
+        long = "--until-date",
+        default_value = "",
+        help = "Only consider issues updated before this date. Format: YYYY-MM-DD"
+    )]
+    pub until_date: String,
+
+    /// --status=status
+    #[structopt(
+        long = "--status",
+        short = "-s",
+        default_value = "",
+        help = "Only consider issues that have a particular status."
+    )]
+    pub status: String,
 
     /// Positional argument
     #[structopt(help = "Required JSON file.")]
