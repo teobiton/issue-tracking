e399a35b0e1095e86b1bc00753a3cb02a8b05a67
teobiton <teo.biton@gmail.com>
2023-07-07 18:37:17 +0200

    Add state filter test

--- tests/test_filters.rs
+++ tests/test_filters.rs
@@ -165,7 +165,92 @@ fn test_label_filtering() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn test_date_filtering() -> Result<(), Box<dyn std::error::Error>> {
+fn test_start_date_filtering() -> Result<(), Box<dyn std::error::Error>> {
+    let start_date: Date = Date {
+        year: 2020,
+        month: 6,
+        day: 15,
+    };
+
+    let end_date: Date = Date {
+        year: 2023,
+        month: 8,
+        day: 12,
+    };
+
+    let filter: Filters = Filters {
+        label_filter: LabelFilter {
+            active: false,
+            pattern: String::from(""),
+        },
+
+        state_filter: StateFilter {
+            active: false,
+            pattern: String::from(""),
+        },
+
+        date_filter: DateFilter {
+            start_active: true,
+            start_date: start_date,
+            end_active: false,
+            end_date: end_date,
+        },
+    };
+
+    let issue_a: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2020-06-16"),
+        updated_at: String::from("2020-06-16"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_b: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2022-06-15"),
+        updated_at: String::from("2022-06-15"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_c: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2020-06-11"),
+        updated_at: String::from("2020-06-11"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_d: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2012-08-14"),
+        updated_at: String::from("2012-08-14"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issues: [Issue; 4] = [issue_a, issue_b, issue_c, issue_d];
+
+    let expected: [bool; 4] = [false, false, true, true];
+
+    for n in 0..4 {
+        println!("{} == {}", filter.reject(&issues[n]), expected[n]);
+        assert_eq!(filter.reject(&issues[n]), expected[n]);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_end_date_filtering() -> Result<(), Box<dyn std::error::Error>> {
     let start_date: Date = Date {
         year: 2020,
         month: 6,
@@ -232,6 +317,176 @@ fn test_date_filtering() -> Result<(), Box<dyn std::error::Error>> {
         number: 1,
         labels: Vec::new(),
         state: String::from(""),
+        created_at: String::from("2023-08-14"),
+        updated_at: String::from("2023-08-14"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issues: [Issue; 4] = [issue_a, issue_b, issue_c, issue_d];
+
+    let expected: [bool; 4] = [false, false, true, true];
+
+    for n in 0..4 {
+        println!("{} == {}", filter.reject(&issues[n]), expected[n]);
+        assert_eq!(filter.reject(&issues[n]), expected[n]);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_inbetween_date_filtering() -> Result<(), Box<dyn std::error::Error>> {
+    let start_date: Date = Date {
+        year: 2020,
+        month: 6,
+        day: 15,
+    };
+
+    let end_date: Date = Date {
+        year: 2023,
+        month: 8,
+        day: 12,
+    };
+
+    let filter: Filters = Filters {
+        label_filter: LabelFilter {
+            active: false,
+            pattern: String::from(""),
+        },
+
+        state_filter: StateFilter {
+            active: false,
+            pattern: String::from(""),
+        },
+
+        date_filter: DateFilter {
+            start_active: true,
+            start_date: start_date,
+            end_active: true,
+            end_date: end_date,
+        },
+    };
+
+    let issue_a: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2020-06-16"),
+        updated_at: String::from("2020-06-16"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_b: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2022-06-15"),
+        updated_at: String::from("2022-06-15"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_c: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2024-06-15"),
+        updated_at: String::from("2024-06-15"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_d: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from(""),
+        created_at: String::from("2023-08-14"),
+        updated_at: String::from("2023-08-14"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issues: [Issue; 4] = [issue_a, issue_b, issue_c, issue_d];
+
+    let expected: [bool; 4] = [false, false, true, true];
+
+    for n in 0..4 {
+        println!("{} == {}", filter.reject(&issues[n]), expected[n]);
+        assert_eq!(filter.reject(&issues[n]), expected[n]);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_state_filtering() -> Result<(), Box<dyn std::error::Error>> {
+    let start_date: Date = Date {
+        year: 2020,
+        month: 6,
+        day: 15,
+    };
+
+    let end_date: Date = Date {
+        year: 2023,
+        month: 8,
+        day: 12,
+    };
+
+    let filter: Filters = Filters {
+        label_filter: LabelFilter {
+            active: false,
+            pattern: String::from(""),
+        },
+
+        state_filter: StateFilter {
+            active: true,
+            pattern: String::from("opened"),
+        },
+
+        date_filter: DateFilter {
+            start_active: false,
+            start_date: start_date,
+            end_active: false,
+            end_date: end_date,
+        },
+    };
+
+    let issue_a: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from("opened"),
+        created_at: String::from("2020-06-16"),
+        updated_at: String::from("2020-06-16"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_b: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from("opened"),
+        created_at: String::from("2022-06-15"),
+        updated_at: String::from("2022-06-15"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_c: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from("closed"),
+        created_at: String::from("2024-06-15"),
+        updated_at: String::from("2024-06-15"),
+        closed_at: Some(String::from("")),
+    };
+
+    let issue_d: Issue = Issue {
+        title: String::from(""),
+        number: 1,
+        labels: Vec::new(),
+        state: String::from("closed"),
         created_at: String::from("2023-08-14"),
         updated_at: String::from("2023-08-14"),
         closed_at: Some(String::from("")),
